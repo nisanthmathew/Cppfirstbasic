4520aa5c6837f6a77c14bbb1c207bbbe2589cdbe
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-02-04 12:30:43 +0100

    "new udemy tutorial 1 for Qtests"

---
+++ Qtqtesttut1/udemytut1.cpp
@@ -0,0 +1,6 @@
+#include "udemytut1.h"
+
+udemytut1::udemytut1()
+{
+
+}
---
+++ Qtqtesttut1/udemytut1.h
@@ -0,0 +1,13 @@
+#ifndef UDEMYTUT1_H
+#define UDEMYTUT1_H
+
+#include <QObject>
+
+class udemytut1
+{
+    Q_OBJECT
+public:
+    udemytut1();
+};
+
+#endif // UDEMYTUT1_H
