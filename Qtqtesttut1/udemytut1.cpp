b1273422ae85626dc619114117d86b080d5e8c44
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-02-04 12:14:03 +0100

    removed old test project

---
+++ Qtqtesttut1/udemytut1.cpp
@@ -0,0 +1,6 @@
+#include "udemytut1.h"
+#include <QtTest/QTest>
+udemytut1::udemytut1()
+{
+
+}
---
+++ Qtqtesttut1/udemytut1.h
@@ -0,0 +1,11 @@
+#ifndef UDEMYTUT1_H
+#define UDEMYTUT1_H
+
+
+class udemytut1
+{
+public:
+    udemytut1();
+};
+
+#endif // UDEMYTUT1_H
