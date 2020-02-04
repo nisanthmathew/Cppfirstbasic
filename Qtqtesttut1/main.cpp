6548959903af2b0e54f8367baaf690e26787d040
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-02-04 09:30:36 +0100

    prints hello world

--- Qtqtesttut1/main.cpp
+++ Qtqtesttut1/main.cpp
@@ -1,10 +1,11 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    qDebug() << "hello git test";
     MainWindow w;
     w.show();
     return a.exec();
