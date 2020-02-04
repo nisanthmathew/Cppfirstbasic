0fcbaf2e9bc22bbec9415fd6473254adf2905c1f
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-02-04 09:58:49 +0100

    print git test 2

--- Qtqtesttut1/main.cpp
+++ Qtqtesttut1/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     qDebug() << "hello git test";
+    qDebug() << "git test 2";
     MainWindow w;
     w.show();
     return a.exec();
