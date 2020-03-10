51b051c1916476bb2768c81e0472b0fc0d753820
nisanth mathew james <nisanthmathewjames@gmail.com>
2020-03-10 09:11:47 +0100

    added move assignment

--- Qtqtesttut1/mystring.cpp
+++ Qtqtesttut1/mystring.cpp
@@ -2,12 +2,14 @@
 
 MyString::MyString(QObject *parent) : QObject(parent), mystr{nullptr} // empty mystring init
 {
+    std::cout << "no args constructor called" << std::endl;
     mystr = new char[1];
     *mystr = '\0';
 }
 
 MyString::MyString(const char* normalstring): mystr{nullptr}
 {
+    std::cout << "overloaded constructor called" << std::endl;
     if(normalstring==nullptr){ //checking for empty string
         mystr = new char[1];
         *mystr = '\0';
@@ -20,8 +22,15 @@ MyString::MyString(const char* normalstring): mystr{nullptr}
 
 MyString::MyString(MyString &copystring): mystr{nullptr} // string copy
 {
+    std::cout << "copy constructor called" << std::endl;
     mystr = new char[strlen(copystring.mystr)+1];
     strcpy(mystr,copystring.mystr);
+}
+
+MyString::MyString(MyString &&movestring): mystr{movestring.mystr} // move constructor
+{
+    std::cout << "move constructor called" << std::endl;
+    movestring = nullptr;
 }
 
 MyString::~MyString()
@@ -41,6 +50,7 @@ const char* MyString::getmystring() const
 
 MyString &MyString::operator=(const MyString &rhs)
 {
+    std::cout << "copy assignment called" << std::endl;
     if(this == &rhs){
         return *this; // return the object if the rhs is the same object
     }
@@ -50,6 +60,19 @@ MyString &MyString::operator=(const MyString &rhs)
     strcpy(this->mystr,rhs.mystr);
     return *this;
 
+}
+
+MyString &MyString::operator=(MyString &&rhs)
+{
+    std::cout << "move assignment called" << std::endl;
+    if(this == &rhs){
+        return *this;
+    }
+
+    delete [] this->mystr;
+    this->mystr = rhs.mystr;
+    rhs.mystr = nullptr;
+    return *this;
 }
 
 void MyString::display(std::ostream& outstream) const // display the sting
--- Qtqtesttut1/mystring.h
+++ Qtqtesttut1/mystring.h
@@ -12,12 +12,15 @@ public:
     explicit MyString(QObject *parent = nullptr); // no arg constructor
     MyString(const char* normalstr); // overloaded constructor
     MyString(MyString &copystring); // copy constructor
+    MyString(MyString &&movestring); //move constructor
     ~MyString();
 
     void display(std::ostream& outstream) const; // display string
     int getlength() const; // display string length
     const char* getmystring() const; // get string
     MyString &operator=(const MyString &rhs); // assignment operator overloading
+    MyString &operator=(MyString &&rhs); //move operator overloading
+
 signals:
 
 };
